1409fb11c9ecf1a7c8f2d64a7fcc59bcbcce813d
Roman <77742141+rumpear@users.noreply.github.com>
2022-06-22 19:35:20 +0300

    upd services

--- src/services/contactsAPI.js
+++ src/services/contactsAPI.js
@@ -1,24 +1,16 @@
 import axios from 'axios';
-import { token } from './authApi';
-
-// axios.defaults.baseURL = 'https://connections-api.herokuapp.com/';
 
 export const getContacts = async () => {
   const { data } = await axios.get('/contacts');
-  console.log(data.token, 'getContacts');
-  console.log(data);
-  token.set(data.token);
   return data;
 };
 
 export const createContact = async values => {
   const { data } = await axios.post('/contacts', values);
-  token.set(data.token);
   return data;
 };
 
 export const deleteContact = async id => {
   const { data } = await axios.delete(`/contacts/${id}`);
-  token.set(data.token);
   return data;
 };
