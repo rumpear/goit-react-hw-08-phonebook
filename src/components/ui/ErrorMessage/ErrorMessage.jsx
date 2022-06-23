9aa2f1e51fb0a8dad7836de2634475dcbedcde09
Roman <77742141+rumpear@users.noreply.github.com>
2022-06-23 20:01:58 +0300

    delete useless components

--- src/components/ui/ErrorMessage/ErrorMessage.jsx
+++
@@ -1,9 +0,0 @@
-import PropTypes from 'prop-types';
-
-export const ErrorMessage = ({ message }) => {
-  return <p>Something went wrong: {message}. Please reload the page</p>;
-};
-
-ErrorMessage.propTypes = {
-  message: PropTypes.string.isRequired,
-};
