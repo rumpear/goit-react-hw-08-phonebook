830521091fb6aff0061937b2272e249c6eaa6d08
Roman <77742141+rumpear@users.noreply.github.com>
2022-06-26 21:25:53 +0300

    upd ContactsItem

--- src/components/ContactsList/ContactsItem/ContactsItem.jsx
+++ src/components/ContactsList/ContactsItem/ContactsItem.jsx
@@ -6,10 +6,22 @@ import { useState } from 'react';
 import { removeContact } from '../../../redux/contacts/contactsOperations';
 import { getErrorValue } from '../../../redux/contacts/contactsSelectors';
 import { Loader } from '../../ui/Loader';
-import { Button, Item, Text } from './ContactsItem.styled';
+import { Button, Item, Name, Phone } from './ContactsItem.styled';
+import {
+  Card,
+  CardActions,
+  CardContent,
+  IconButton,
+  Typography,
+} from '@mui/material';
 // import { useEffect } from 'react';
 
-export const ContactsItem = ({ id, name, number, contacts }) => {
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ShareIcon from '@mui/icons-material/Share';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+
+export const ContactsItem = ({ id, name, number }) => {
   const dispatch = useDispatch();
   const error = useSelector(getErrorValue);
   const [loading, setLoading] = useState(false);
@@ -23,10 +35,16 @@ export const ContactsItem = ({ id, name, number, contacts }) => {
     }
   };
 
+  console.log('34532452345324523453423'.length, 'length');
+
   return (
     <Item>
-      <Text>{name}</Text>
-      <Text>{number}</Text>
+      <div>
+        <Name>{name}</Name>
+        <Phone href={`tel:${number}`} type="tel">
+          {number}
+        </Phone>
+      </div>
       <Button
         type="button"
         disabled={loading || error}
@@ -35,6 +53,40 @@ export const ContactsItem = ({ id, name, number, contacts }) => {
         {loading ? <Loader size={15} /> : <VscClose size={20} />}
       </Button>
     </Item>
+  );
+
+  return (
+    <Card
+      sx={{
+        maxWidth: 345,
+        mb: 1.5,
+        '&:last-child': {
+          mb: 0,
+        },
+      }}
+    >
+      <CardContent
+        sx={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          '&:last-child': { p: '16px' },
+        }}
+      >
+        <div>
+          <Typography sx={{ fontSize: 18 }} color="text.primary" gutterBottom>
+            {name}
+          </Typography>
+          <Typography sx={{ mb: 0 }} color="text.secondary">
+            {number}
+          </Typography>
+        </div>
+        <CardActions disableSpacing sx={{ p: 0 }}>
+          <IconButton aria-label="add to favorites">
+            <FavoriteIcon />
+          </IconButton>
+        </CardActions>
+      </CardContent>
+    </Card>
   );
 };
 
--- src/components/ContactsList/ContactsItem/ContactsItem.styled.jsx
+++ src/components/ContactsList/ContactsItem/ContactsItem.styled.jsx
@@ -2,19 +2,50 @@ import styled from 'styled-components';
 
 export const Item = styled.li`
   font-size: 17px;
-  margin-bottom: 10px;
+  margin-bottom: 15px;
+  padding: 18px;
 
   display: flex;
   justify-content: space-between;
   align-items: center;
 
+  /* max-width: 300px; */
+  width: 275px;
+  /* text-overflow: ellipsis;
+  white-space: nowrap;
+  overflow: hidden; */
+
+  background-color: #fff;
+  color: rgba(0, 0, 0, 0.87);
+  -webkit-transition: box-shadow 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+  transition: box-shadow 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+  border-radius: 4px;
+  box-shadow: 0px 2px 1px -1px rgb(0 0 0 / 20%),
+    0px 1px 1px 0px rgb(0 0 0 / 14%), 0px 1px 3px 0px rgb(0 0 0 / 12%);
+
   :last-child {
     margin-bottom: 0;
   }
+
+  @media (min-width: 768px) {
+    margin: 0;
+  }
 `;
 
-export const Text = styled.p`
+export const Name = styled.p`
   font-size: 17px;
+  margin-bottom: 10px;
+  text-align: left;
+  /* text-overflow: ellipsis;
+  white-space: nowrap;
+  overflow: hidden; */
+`;
+
+export const Phone = styled.a`
+  font-size: 17px;
+  color: black;
+  display: block;
+  text-align: left;
 `;
 
 export const Button = styled.button`
@@ -24,6 +55,8 @@ export const Button = styled.button`
   align-items: center;
   width: 30px;
   height: 30px;
+  margin-left: 10px;
+  /* padding: 10px; */
 
   background-color: #f5f4fa;
   box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.15);
