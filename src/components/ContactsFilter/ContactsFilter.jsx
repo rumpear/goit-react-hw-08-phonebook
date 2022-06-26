9cad193be52b2246e15295aea6ce5c5e6bcee35b
Roman <77742141+rumpear@users.noreply.github.com>
2022-06-26 21:25:08 +0300

    upd ContactsFilter

--- src/components/ContactsFilter/ContactsFilter.jsx
+++ src/components/ContactsFilter/ContactsFilter.jsx
@@ -1,7 +1,9 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { updateFilter } from '../../redux/contacts/contactsAsyncSlice';
 import { getFilterValue } from '../../redux/contacts/contactsSelectors';
-import { Title, Input } from './ContactsFilter.styled';
+import { Title } from './ContactsFilter.styled';
+import SearchIcon from '@mui/icons-material/Search';
+import { InputAdornment, TextField } from '@mui/material';
 
 export const ContactsFilter = () => {
   const filterValue = useSelector(getFilterValue);
@@ -14,12 +16,20 @@ export const ContactsFilter = () => {
   return (
     <div>
       <Title>Find contacts by name</Title>
-      <Input
+      <TextField
+        sx={{ width: '100%' }}
         type="text"
         name="filter"
-        required
+        placeholder="Search..."
         value={filterValue}
         onChange={handleSearchContact}
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <SearchIcon />
+            </InputAdornment>
+          ),
+        }}
       />
     </div>
   );
