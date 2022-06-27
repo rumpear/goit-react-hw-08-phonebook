c5b6dc52099618389af6ac97b1709e409540b6a3
Roman <77742141+rumpear@users.noreply.github.com>
2022-06-27 22:29:25 +0300

    added lazy code splitting

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,6 @@
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-// import { Section } from './ui/Section';
-// import { ContactsFilter } from './ContactsFilter';
-// import { ContactsForm } from './ContactsForm';
-// import { ContactsList } from './ContactsList';
-
-// import { Wrapper, TitlePhonebook, TitleContacts } from './App.styled';
-import { ContactsPage, HomePage, LoginPage, RegisterPage } from '../pages';
-
 import { Routes, Route } from 'react-router-dom';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
@@ -16,6 +8,13 @@ import { getCurrentUser } from '../redux/auth/authOperations';
 import { PublicRoute } from './Routes';
 import { PrivateRoute } from './Routes/PrivateRoute';
 import { useSelector } from 'react-redux';
+import LinearProgress from '@mui/material/LinearProgress';
+import { lazy, Suspense } from 'react';
+
+const HomePage = lazy(() => import('../pages/HomePage/'));
+const LoginPage = lazy(() => import('../pages/LoginPage'));
+const RegisterPage = lazy(() => import('../pages/RegisterPage'));
+const ContactsPage = lazy(() => import('../pages/ContactsPage'));
 
 export const App = () => {
   const dispatch = useDispatch();
@@ -28,96 +27,53 @@ export const App = () => {
     state => state.auth.isLoadingCurrentUser
   );
 
-  if (isLoadingCurrentUser) {
-    return <h1>Loading</h1>;
-  }
+  if (isLoadingCurrentUser) return <LinearProgress />;
 
   return (
     <>
-      <Routes>
-        <Route path="/" element={<HomePage />}>
+      <Suspense fallback={<LinearProgress />}>
+        <Routes>
+          <Route path="/" element={<HomePage />}>
+            <Route
+              index
+              element={
+                <PublicRoute redirectTo="/contacts">
+                  <LoginPage />
+                </PublicRoute>
+              }
+            />
+            <Route
+              path="registration"
+              element={
+                <PublicRoute redirectTo="/contacts">
+                  <RegisterPage />
+                </PublicRoute>
+              }
+            />
+          </Route>
           <Route
-            index
+            path="/contacts"
             element={
-              <PublicRoute redirectTo="/contacts">
-                <LoginPage />
-              </PublicRoute>
+              <PrivateRoute>
+                <ContactsPage />
+              </PrivateRoute>
             }
           />
-          <Route
-            path="registration"
-            element={
-              <PublicRoute redirectTo="/contacts">
-                <RegisterPage />
-              </PublicRoute>
-            }
-          />
-        </Route>
-        <Route
-          path="/contacts"
-          element={
-            <PrivateRoute>
-              <ContactsPage />
-            </PrivateRoute>
-          }
-        />
-        <Route path="*" element={<h1>Not found</h1>} />
-      </Routes>
+          <Route path="*" element={<h1>Not found</h1>} />
+        </Routes>
 
-      <ToastContainer
-        position="top-center"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-      />
+        <ToastContainer
+          position="top-center"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
+      </Suspense>
     </>
   );
 };
-
-// return (
-//   <>
-//     {isLoadingCurrentUser ? (
-//       <p>Loading</p>
-//     ) : (
-//       <Routes>
-//         <Route path="/" element={<HomePage />}>
-//           <Route
-//             index
-//             path="login"
-//             element={
-//               <PublicRoute redirectTo="/contacts">
-//                 <LoginPage />
-//               </PublicRoute>
-//             }
-//           />
-//           <Route path="registration" element={<RegisterPage />} />
-//         </Route>
-//         <Route
-//           path="/contacts"
-//           element={
-//             <PrivateRoute redirectTo="/login">
-//               <ContactsPage />
-//             </PrivateRoute>
-//           }
-//         />
-//         <Route path="*" element={<h1>Not found</h1>} />
-//       </Routes>
-//     )}
-//     <ToastContainer
-//       position="top-center"
-//       autoClose={5000}
-//       hideProgressBar={false}
-//       newestOnTop={false}
-//       closeOnClick
-//       rtl={false}
-//       pauseOnFocusLoss
-//       draggable
-//       pauseOnHover
-//     />
-//   </>
-// );
