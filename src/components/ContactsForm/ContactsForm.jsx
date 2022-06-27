845b23c1af0ff623816a358eb414f6e743ae04ed
Roman <77742141+rumpear@users.noreply.github.com>
2022-06-27 22:24:50 +0300

    upd ContactsForm

--- src/components/ContactsForm/ContactsForm.jsx
+++ src/components/ContactsForm/ContactsForm.jsx
@@ -2,18 +2,7 @@ import { Controller, useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { IMaskInput } from 'react-imask';
 import { toast } from 'react-toastify';
-import { ClipLoader } from 'react-spinners';
-
-import { forwardRef, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { addContact } from '../../redux/contacts/contactsOperations';
 import {
-  getContactsValue,
-  getErrorValue,
-} from '../../redux/contacts/contactsSelectors';
-import { Button } from './ContactsForm.styled';
-import {
-  // Button,
   TextField,
   Box,
   FormControl,
@@ -22,6 +11,14 @@ import {
   FormHelperText,
 } from '@mui/material';
 import schema from '../../utils/schemes';
+import { LoadingButton } from '../ui/LoadingButton';
+import { forwardRef, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addContact } from '../../redux/contacts/contactsOperations';
+import {
+  getContactsValue,
+  getErrorValue,
+} from '../../redux/contacts/contactsSelectors';
 
 export const ContactsForm = () => {
   const [isLoading, setIsLoading] = useState(false);
@@ -46,7 +43,6 @@ export const ContactsForm = () => {
   };
 
   const onSubmit = ({ name, phone }) => {
-    console.log(phone.length);
     if (checkDuplicateName(name)) {
       return toast.warn(
         'You are trying to enter a name that is already on the phonebook'
@@ -94,73 +90,49 @@ export const ContactsForm = () => {
   });
 
   return (
-    <>
-      <Box
-        component="form"
-        onSubmit={handleSubmit(onSubmit)}
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          '& .MuiFormControl-root': { m: 1, width: '100%' },
-          // '& > button': { m: 1 },
-        }}
-      >
-        <TextField
-          type="text"
-          label="Name"
-          // margin="normal"
-          error={!!errors.name}
-          helperText={errors.name?.message}
-          placeholder="Enter the name of your contact"
-          {...register('name')}
-        />
-        <Controller
-          name="phone"
-          control={control}
-          render={({ field }) => (
-            <FormControl error={!!errors.phone}>
-              <InputLabel htmlFor="phone-input">Phone</InputLabel>
-              <OutlinedInput
-                id="phone-input"
-                name="phone"
-                label="phone"
-                placeholder="(063) 00 00 000"
-                inputComponent={TextMaskCustom}
-                {...field}
-              />
-              <FormHelperText id="phone-input">
-                {errors.phone?.message}
-              </FormHelperText>
-            </FormControl>
-          )}
-        />
-        <Button type="submit" disabled={error || isLoading}>
-          {isLoading ? (
-            <>
-              <ClipLoader
-                size={20}
-                css={`
-                  margin-right: 10px;
-                  border-color: #82878a;
-                `}
-              />
-              Add contact
-            </>
-          ) : (
-            'Add contact'
-          )}
-        </Button>
-        {/* <LoadingButton
-          onClick={handleSubmit(onSubmit)}
-          endIcon={<SendIcon />}
-          loading={isLoading}
-          loadingPosition="end"
-          variant="contained"
-        >
-          Send
-        </LoadingButton> */}
-      </Box>
-    </>
+    <Box
+      component="form"
+      onSubmit={handleSubmit(onSubmit)}
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        '& .MuiFormControl-root': { m: 1, width: '100%' },
+      }}
+    >
+      <TextField
+        type="text"
+        label="Name"
+        error={!!errors.name}
+        helperText={errors.name?.message}
+        placeholder="Enter the name of your contact"
+        {...register('name')}
+      />
+      <Controller
+        name="phone"
+        control={control}
+        render={({ field }) => (
+          <FormControl error={!!errors.phone}>
+            <InputLabel htmlFor="phone-input">Phone</InputLabel>
+            <OutlinedInput
+              id="phone-input"
+              name="phone"
+              label="phone"
+              placeholder="(063) 00 00 000"
+              inputComponent={TextMaskCustom}
+              {...field}
+            />
+            <FormHelperText id="phone-input">
+              {errors.phone?.message}
+            </FormHelperText>
+          </FormControl>
+        )}
+      />
+      <LoadingButton
+        text="Add contact"
+        error={error}
+        isLoading={isLoading}
+      ></LoadingButton>
+    </Box>
   );
 };
